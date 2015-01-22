da64f089f4e78d2cef69ce6cc82e6c1cb22595d6
Blake Johnson <blake41@gmail.com>
2015-01-22 09:10:29 -0500

    done

---
+++ grouper.rb
@@ -0,0 +1,37 @@
+array = ['Paul Han',
+'Inga Nikitina',
+'Brian Choy',
+'Andrei Motinga',
+'Mubiyna Casey',
+'Geraldina Alvarez Garcia',
+'Stephanie Aviles',
+'Lewaa Bahmad',
+'Maria Antropova',
+'Jayana Johnson',
+'Keith Embler',
+'Ming Zhang',
+'Tiffany Haltom',
+'Jordan Falcon',
+'Sanjay Chakravorty',
+'Anton Halim',
+'Hessvacio Hassan',
+'Mykle Nero',
+'Earl Clifton',
+'Shawn Hansen',
+'Danielle Hill',
+'Sebastian King',
+'Andrew Cabrera',
+'Daniel Ferrer',
+'Lindsay Padian',
+'Lezou Dali',
+'Eric Orellana',
+'Ashley Chu',
+'david lorente',
+'Daniel Aksenov',
+'Leah Suter',
+'In Ho Hwang']
+8.times do |i|
+  puts i
+  puts i*4
+  #puts array.slice(i,i*4)
+end
